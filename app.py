0aa7a37d71bdfdedaa2a8fda7602ad2274b9ed94
Matteo Orlando <matteo.orlando.1993@gmail.com>
2023-01-08 13:28:18 +0100

    First
    First version

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import json
+import requests
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.json
+    headers = {"Accept": "application/citeproc+json"}
+    r = requests.get(data["url"], headers=headers)
+    output = r.json()
+    return json.dumps(output)
